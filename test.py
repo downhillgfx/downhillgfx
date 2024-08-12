1d50678234a58e767cbed257914d21838db79390
DownhillGFX <125319966+downhillgfx@users.noreply.github.com>
2024-08-12 02:39:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import time
 
-print("Starting")
-time.sleep(2)
-user = input("Enter your username")
-time.sleep(1)
-pass = input("Enter the password")
-print("Processing Request")
-if user = "Downhillgfx" then:
-    print("username confirmed.
-time.sleep(2)
